d7fac97a321b810eaea423d7ffd63606ee2adbd9
MehfoozurRehman <mehfoozijaz786@gmail.com>
2020-12-17 23:18:59 +0500

    commet

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+function Active() {
+  var x = document.getElementById("myLinks");
+  if(x.style.display === "flex") {
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "flex";
+  }
+}
