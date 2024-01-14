ea708651764a68032cda5f38c9a4c843eaa5acb7
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-01-14 22:31:50 +0500

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,13 @@
 const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
+
 menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-  }
+  menuBtn.classList.toggle("open");
+  menuOpen = !menuOpen;
 });
-function Active() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "flex";
-  }
+
+function toggleMenu() {
+  const x = document.getElementById("myLinks");
+  x.style.display = menuOpen ? "none" : "flex";
+  menuOpen = !menuOpen;
 }
